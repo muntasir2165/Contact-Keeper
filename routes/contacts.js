640e6096520a0b63870f2f21c9d118b7de2cb869
Muntasir Biojid <muntasir2165@hotmail.com>
2020-02-02 14:05:28 -0800

    Updated express-validator imports to address deprecation warning

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { check, validationResult } = require('express-validator/check');
+// const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 const Contact = require('../models/Contact');
